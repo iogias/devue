d40c3e706f4b32b7ea4b0bc5f3b13d1dc83d18db
iogias <iogias.box@gmail.com>
2021-03-21 23:50:07 +0700

    initial reaction-timer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showResult" :score="score" />
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{Block, Results},
+  data(){
+    return{
+      isPlaying:false,
+      delay:null,
+      score:null,
+      showResult:false
+    }
+  },
+  methods:{
+    start(){
+      this.delay = 1000 + Math.random() * 5000
+      this.isPlaying= true
+      this.showResult = false
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResult = true
+    }
   }
 }
 </script>
@@ -22,5 +43,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button {
+  background-color: teal;
+  color: whitesmoke;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 14px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,33 @@
+<template>
+    <p>Reaction time : {{ score }} ms</p>
+    <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props:["score"],
+  data(){
+    return {
+      rank:null
+    }
+  },
+  mounted(){
+    if(this.score < 250) {
+      this.rank = 'Ninja finger'
+    } else if(this.score < 400) {
+      this.rank = 'Good reflexes'
+    } else {
+      this.rank = 'Snail pace ..'
+    }
+  }
+}
+</script>
+
+<style>
+.rank {
+  color:teal;
+  font-size: 1.2em;
+  padding: 5px;
+  font-weight: bold;
+}
+</style>
